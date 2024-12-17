b62a713f57b783789cae7ab65b84773157c8c93b
mkchunmengalf@gmail.com <mkchunmengalf@gmail.com>
2024-12-17 16:42:13 +0000

    Created Form1.ItemTemplate1.ItemTemplate2; Edited settings, Form1.ItemTemplate1 and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,8 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 
 
 class Form1(Form1Template):
@@ -8,3 +11,24 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+    self.menu_dict = []
+    counter = 0
+    for row in app_tables.test.search():
+      if counter % 3 == 0:
+        row_dict = {"nameLeft":row['name'],"imageLeft":row['image'],"descriptionLeft":row['description']}
+      elif counter % 3 == 1:
+        row_dict["nameCenter"] = row['name']
+        row_dict["imageCenter"] = row["image"]
+        row_dict["descriptionCenter"] = row['description']
+      elif counter % 3 == 2:
+        row_dict["nameRight"] = row['name']
+        row_dict["imageRight"] = row["image"]
+        row_dict["descriptionRight"] = row['description']
+        self.menu_dict.append(row_dict)
+      counter += 1
+    self.repeating_panel_1.items = self.menu_dict
+    
+        
+      
+    
+    
