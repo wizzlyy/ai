b166848e5a339bc9367bb49549a886aa2d24aac9
mkchunmengalf@gmail.com <mkchunmengalf@gmail.com>
2024-12-29 06:56:36 +0000

    Edited Menu.ItemPage

--- client_code/Menu/ItemPage/__init__.py
+++ client_code/Menu/ItemPage/__init__.py
@@ -7,6 +7,7 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from anvil_extras import animation
 import time
 
 class ItemPage(ItemPageTemplate):
@@ -18,13 +19,23 @@ class ItemPage(ItemPageTemplate):
     self.prices = prices
     self.addImage = addImage
     
-    self.counter = 0
+    self.counter = -1
     
     self.addImage.append(self.image)
     self.init_components(**properties)
     # Any code you write here will run before the form opens.
+    self.fade_in = animation.Transition(opacity=[0, 1])
+    self.fade_out = reversed(self.fade_in)
     
   def imageTimer_tick(self, **event_args):
     """This method is called Every [interval] seconds. Does not trigger if [interval] is 0."""
-    self.imageBox.source = self.addImage[self.counter%3]
+    self.imageBox.visible = True
+    animation.animate(self.imageBox, self.fade_in, 250)
+    time.sleep(4.49)
+    animation.animate(self.imageBox, self.fade_out, 250)
+    time.sleep(0.5)
+    self.imageBox.visible = False
     self.counter += 1
+    self.imageBox.source = self.addImage[self.counter%3]
+    
+    
