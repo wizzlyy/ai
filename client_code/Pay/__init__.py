257deca2df660de484d64e5bb15771c815842a4d
mkchunmengalf@gmail.com <mkchunmengalf@gmail.com>
2025-02-13 13:16:15 +0000

    Edited Pay

--- client_code/Pay/__init__.py
+++ client_code/Pay/__init__.py
@@ -15,5 +15,29 @@ class Pay(PayTemplate):
     self.init_components(**properties)
     stripe.api_key = anvil.server.call('getKey')
     # Any code you write here will run before the form opens.
+    self.totalLabel.text = "All blanks need to be filled in!"
 
+  def cBox_change(self, **event_args):
+    """This method is called when the text in this text box is edited"""
+    if not self.cBox.text.isdigit():
+      self.cWarning.text = "Input needs to be a number"
+    else:
+      self.cWarning.text = ""
+
+  def sBox_change(self, **event_args):
+    """This method is called when the text in this text box is edited"""
+    if not self.sBox.text.isdigit():
+      self.sWarning.text = "Input needs to be a number"
+    else:
+      self.sWarning.text = ""
+
+  def aBox_change(self, **event_args):
+    """This method is called when the text in this text box is edited"""
+    if not self.aBox.text.isdigit():
+      self.aWarning.text = "Input needs to be a number"
+    else:
+      self.aWarning.text = ""
+
+    
+    
   
